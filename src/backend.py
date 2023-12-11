8789abcf4f36f5f05b101a4ad0c2a1301d7b74c2
ValMobYKang <115156261+ValMobYKang@users.noreply.github.com>
2023-12-11 11:17:56 +0100

    Add Bitbucket Resource  (#1)
    * format
    
    * bug fix
    
    * update gitignore
    
    * update
    
    * ignore
    
    * fix logging
    
    * update backend
    
    * format
    
    * update git workflow
    
    * fix format

--- src/backend.py
+++ src/backend.py
@@ -1,6 +1,4 @@
 import os
-import phoenix as px
-import llama_index
 from llama_index import (
     VectorStoreIndex,
     ServiceContext,
@@ -10,83 +8,161 @@ from llama_index import (
 from llama_index.llms import OpenAI
 from llama_index.prompts import PromptTemplate
 from llama_index.embeddings import HuggingFaceEmbedding
-from llama_index.node_parser import SimpleNodeParser
-from llama_index.text_splitter import TokenTextSplitter
 from llama_index.indices.prompt_helper import PromptHelper
-from utils import ConfluenceReader, SentenceTransformerRerank
+from llama_index.query_engine import CustomQueryEngine
+from llama_index.retrievers import BaseRetriever
+from llama_index.response_synthesizers import (
+    get_response_synthesizer,
+    BaseSynthesizer,
+)
+from llama_index.postprocessor.types import BaseNodePostprocessor
+from llama_index.schema import QueryBundle
+from llama_index.callbacks import CallbackManager
+
+import phoenix as px
+from phoenix.trace.llama_index import (
+    OpenInferenceTraceCallbackHandler,
+)
+
+from typing import Literal
+from utils import ConfluenceReader, SentenceTransformerRerank, BitbucketReader
 
 os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_API_KEY"
 os.environ["OPENAI_API_BASE"] = "http://localhost:8000/v1"
 
-LLM = OpenAI(temperature=0.1, max_tokens=2048)
-EMBEDDING = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-RERANK = SentenceTransformerRerank(
-    model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=3
-)
+session = px.launch_app()
+cb_manager = CallbackManager(handlers=[OpenInferenceTraceCallbackHandler()])
 
 
-def init_index():
-    if os.path.exists("store"):
-        index = load_index_from_storage(
-            storage_context=StorageContext.from_defaults(persist_dir="store"),
-            service_context=ServiceContext.from_defaults(
-                llm=LLM,
-                embed_model=EMBEDDING,
-                prompt_helper=PromptHelper(chunk_size_limit=2000),
-            ),
+class QueryMultiEngine(CustomQueryEngine):
+    retrievers: list[BaseRetriever]
+    response_synthesizer: BaseSynthesizer
+    node_postprocessors: list[BaseNodePostprocessor]
+
+    def custom_query(self, query_str: str):
+        nodes = []
+        for retriever in self.retrievers:
+            nodes += retriever.retrieve(query_str)
+        for postprocessor in self.node_postprocessors:
+            nodes = postprocessor.postprocess_nodes(
+                nodes=nodes, query_bundle=QueryBundle(query_str)
+            )
+
+        response_obj = self.response_synthesizer.synthesize(query_str, nodes)
+
+        return response_obj
+
+
+def service_context():
+    LLM = OpenAI(temperature=0.1, max_tokens=2048, callback_manager=cb_manager)
+    EMBEDDING = HuggingFaceEmbedding(
+        model_name="BAAI/bge-base-en-v1.5", callback_manager=cb_manager
+    )
+    return ServiceContext.from_defaults(
+        llm=LLM,
+        chunk_size=512,
+        chunk_overlap=20,
+        embed_model=EMBEDDING,
+        prompt_helper=PromptHelper(chunk_size_limit=1000),
+        callback_manager=cb_manager,
+    )
+
+
+def init_index(persist_dir: Literal["confluence_store", "bitbucket_store"]):
+    if os.path.exists(persist_dir):
+        print(f"Loading {persist_dir} ...")
+        return load_index_from_storage(
+            storage_context=StorageContext.from_defaults(persist_dir=persist_dir),
+            service_context=service_context(),
+        )
+
+    if persist_dir == "bitbucket_store":
+        loader = BitbucketReader(
+            project_key=os.environ["BITBUCKET_PROJECT"],
+            base_url=os.environ["BITBUCKET_URL"],
+            branch="master",
+            extensions_to_skip=[
+                ".VIN-decoding",
+                "URL-generalization",
+                "scraping",
+                "FizzBuzz",
+                "Driver-Behaviour",
+                "VIN-OCR",
+                "Sensor-Log",
+                "png",
+                "jpg",
+                "ppm",
+            ],
+        ).load_data()
+    elif persist_dir == "confluence_store":
+        loader = ConfluenceReader(base_url=os.environ["CONFLUENCE_URL"]).load_data(
+            space_key=os.environ["CONFLUENCE_SPACE"],
+            page_status="current",
+            include_attachments=False,
+            max_num_results=10,
         )
     else:
-        index = VectorStoreIndex.from_documents(
-            documents=ConfluenceReader(base_url=os.environ["CONFLUENCE_URL"]).load_data(
-                space_key=os.environ["CONFLUENCE_SPACE"],
-                page_status="current",
-                include_attachments=False,
-                max_num_results=10,
-            ),
-            service_context=ServiceContext.from_defaults(
-                llm=LLM,
-                node_parser=SimpleNodeParser.from_defaults(
-                    text_splitter=TokenTextSplitter(
-                        separator=" ",
-                        chunk_size=512,
-                        chunk_overlap=20,
-                        backup_separators=["\n"],
-                    )
-                ),
-                embed_model=EMBEDDING,
-            ),
-            show_progress=True,
-        )
-        index.storage_context.persist(persist_dir="store")
+        raise Exception("Must have one store")
+
+    index = VectorStoreIndex.from_documents(
+        documents=loader,
+        service_context=service_context(),
+        show_progress=True,
+    )
+    index.storage_context.persist(persist_dir=persist_dir)
+
     return index
 
 
-def get_query_engine(index):
-    return index.as_query_engine(
-        similarity_top_k=5,
-        response_mode="compact",
+def get_query_engine(indices: list):
+    RERANK = SentenceTransformerRerank(
+        model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=3
+    )
+    RERANK.callback_manager = cb_manager
+    dolphin_qa_prompt = PromptTemplate(
+        "<|im_start|>system \n"
+        "You will be presented with context. Your task is to answer the query only based on the context. "
+        "If the context cannot answer the query, you responses 'I don't know' directly without any more responses. \n"
+        "Approach this task step-by-step, take your time. "
+        "This is very important to my career.\n"
+        "The Context information is below. \n"
+        "---------------------\n{context_str}\n--------------------- <|im_end|>\n"
+        "<|im_start|>user \n"
+        "{query_str}<|im_end|> \n"
+        "<|im_start|>assistant"
+    )
+
+    if len(indices) == 1:
+        return indices[0].as_query_engine(
+            similarity_top_k=5,
+            service_context=service_context(),
+            response_mode="compact",
+            node_postprocessors=[RERANK],
+            text_qa_template=dolphin_qa_prompt,
+        )
+
+    return QueryMultiEngine(
+        retrievers=[index.as_retriever(similarity_top_k=5) for index in indices],
         node_postprocessors=[RERANK],
-        text_qa_template=PromptTemplate(
-            "<|im_start|>system \n"
-            "You will be presented with context. You task is to answer the query only based on the context. "
-            "If the context cannot answer the query, you responses 'I don't know'. \n"
-            "Approach this task step-by-step, take your time. \n"
-            "This is very important to my career.<|im_end|>\n"
-            "The Context information is below. \n"
-            "---------------------\n{context_str}\n---------------------\n"
-            "<|im_start|>user \n"
-            "{query_str}<|im_end|> \n"
-            "<|im_start|>assistant"
+        response_synthesizer=get_response_synthesizer(
+            service_context=service_context(),
+            response_mode="compact",
+            text_qa_template=dolphin_qa_prompt,
         ),
+        callback_manager=cb_manager,
     )
 
 
 if __name__ == "__main__":
-    session = px.launch_app()
-    llama_index.set_global_handler("arize_phoenix")
-
-    query_engine = get_query_engine(index=init_index())
     print("[Develop mode]")
+
+    query_engine_bitbucket = get_query_engine(
+        indices=[
+            init_index(persist_dir="bitbucket_store"),
+            init_index(persist_dir="confluence_store"),
+        ]
+    )
+
     while 1:
         question = input("Question: ")
-        print(query_engine.query(question))
+        print(query_engine_bitbucket.query(question))
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
+import os
 import re
+import base64
 from typing import Dict
 import requests
 from functools import lru_cache
@@ -9,6 +11,17 @@ from llama_index.indices.postprocessor import (
 from typing import List, Optional
 from llama_index.callbacks import CBEventType, EventPayload
 from llama_index.schema import MetadataMode, NodeWithScore, QueryBundle
+from dataclasses import dataclass
+
+from llama_index.readers.base import BaseReader
+from llama_index.readers.schema.base import Document
+
+
+@dataclass
+class File:
+    content: str
+    file_name: str
+    url: str
 
 
 class SentenceTransformerRerank(BaseSentenceTransformerRerank):
@@ -219,3 +232,184 @@ def parse_cookie(cookie_file="./cookies.txt"):
                 line_fields = line.strip().split("\t")
                 cookies[line_fields[5]] = line_fields[6]
     return cookies
+
+
+class BitbucketReader(BaseReader):
+    """Bitbucket reader.
+
+    Reads the content of files in Bitbucket repositories.
+
+    """
+
+    def __init__(
+        self,
+        base_url: Optional[str] = None,
+        project_key: Optional[str] = None,
+        branch: Optional[str] = "refs/heads/develop",
+        extensions_to_skip: Optional[List] = [],
+    ) -> None:
+        """Initialize with parameters."""
+        if os.getenv("BITBUCKET_USERNAME") is None:
+            raise ValueError("Could not find a Bitbucket username.")
+        if os.getenv("BITBUCKET_API_KEY") is None:
+            raise ValueError("Could not find a Bitbucket api key.")
+        if base_url is None:
+            raise ValueError("You must provide a base url for Bitbucket.")
+        if project_key is None:
+            raise ValueError("You must provide a project key for Bitbucket repository.")
+        self.base_url = base_url
+        self.project_key = project_key
+        self.branch = branch
+        self.extensions_to_skip = extensions_to_skip
+
+    def get_headers(self):
+        username = os.getenv("BITBUCKET_USERNAME")
+        api_token = os.getenv("BITBUCKET_API_KEY")
+        auth = base64.b64encode(f"{username}:{api_token}".encode()).decode()
+        return {"Authorization": f"Basic {auth}"}
+
+    def get_slugs(self) -> List:
+        """
+        Get slugs of the specific project.
+        """
+        repos_url = (
+            f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/"
+        )
+        headers = self.get_headers()
+        slugs = []
+        response = requests.get(repos_url, headers=headers)
+
+        if response.status_code == 200:
+            repositories = response.json()["values"]
+            for repo in repositories:
+                repo_slug = repo["slug"]
+                slugs.append(repo_slug)
+        return slugs
+
+    def load_all_file_paths(self, slug, branch, directory_path="", paths=[]):
+        """
+        Go inside every file that is present in the repository and get the paths for each file
+        """
+        content_url = f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/{slug}/browse/{directory_path}"
+        query_params = {
+            "at": branch,
+        }
+        headers = self.get_headers()
+        response = requests.get(content_url, headers=headers, params=query_params)
+        response = response.json()
+        if "errors" in response:
+            raise ValueError(response["errors"])
+        children = response["children"]
+        for value in children["values"]:
+            if value["type"] == "FILE":
+                if (
+                    value["path"].get("extension")
+                    and value["path"]["extension"] not in self.extensions_to_skip
+                ):
+                    paths.append(
+                        {
+                            "slug": slug,
+                            "path": f'{directory_path}/{value["path"]["toString"]}',
+                        }
+                    )
+            elif value["type"] == "DIRECTORY":
+                self.load_all_file_paths(
+                    slug=slug,
+                    branch=branch,
+                    directory_path=f'{value["path"]["toString"]}'
+                    if directory_path == ""
+                    else f'{directory_path}/{value["path"]["toString"]}',
+                    paths=paths,
+                )
+
+    def load_text_by_paths(self, slug, file_path, branch) -> List:
+        """
+        Go inside every file that is present in the repository and get the paths for each file
+        """
+        if not file_path.startswith("/"):
+            file_path = "/" + file_path
+        content_url = f"{self.base_url}/rest/api/latest/projects/{self.project_key}/repos/{slug}/browse{file_path}"
+
+        query_params = {
+            "at": branch,
+        }
+        headers = self.get_headers()
+        response = requests.get(content_url, headers=headers, params=query_params)
+        children = response.json()
+        if "errors" in children:
+            # raise ValueError(children["errors"]) # DEBUG
+            return []
+        if "lines" in children:
+            return children["lines"]
+        return []
+
+    def load_text(self, paths) -> List[File]:
+        text_dict = []
+        for path in paths:
+            lines_list = self.load_text_by_paths(
+                slug=path["slug"], file_path=path["path"], branch=self.branch
+            )
+            concatenated_string = ""
+
+            for line_dict in lines_list:
+                text = line_dict.get("text", "")
+                concatenated_string = concatenated_string + " " + text
+
+            file_path = (
+                path["path"] if not path["path"].startswith("/") else path["path"][1:]
+            )
+
+            text_dict.append(
+                File(
+                    content=concatenated_string,
+                    file_name=path["path"].split("/")[-1],
+                    url=f"{self.base_url}/projects/{self.project_key}/repos/{path['slug']}/browse/{file_path}",
+                )
+            )
+        return text_dict
+
+    def load_data(self) -> List[Document]:
+        """Return a list of Document made of each file in Bitbucket."""
+        slugs = self.get_slugs()
+        paths = []
+        for slug in slugs:
+            if slug in ["dsbt"]:
+                continue
+            self.load_all_file_paths(
+                slug=slug, branch=self.branch, directory_path="", paths=paths
+            )
+        print(len(paths))
+        texts = self.load_text(paths)
+        return [
+            Document(
+                text=text.content,
+                extra_info={"file_name": text.file_name, "url": text.url},
+            )
+            for text in texts
+        ]
+
+
+def test_bitbucketReader():
+    loader = BitbucketReader(
+        project_key="MOBDATA",
+        base_url="https://bitbucket.valtech.de",
+        branch="master",
+        extensions_to_skip=[
+            ".VIN-decoding",
+            "URL-generalization",
+            "scraping",
+            "FizzBuzz",
+            "Driver-Behaviour",
+            "VIN-OCR",
+            "Sensor-Log",
+            "png",
+            "jpg",
+        ],
+    )
+
+    documents = loader.load_data()
+    print(len(documents))
+
+
+if __name__ == "__main__":
+    test_bitbucketReader()
